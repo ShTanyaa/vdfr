0e789d248ac4fa914c80677cb0bae4b20857b4b3
ShTanyaa <124123694+ShTanyaa@users.noreply.github.com>
2023-11-17 10:02:48 +0500

    Add files via upload

---
+++ shumatbaev_XOGame/Class1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shumatbaev_XOGame
+{
+    public class Class1
+    {
+        public enum XOElement
+        {
+            None = 0, Cross = 1, Circle = -1
+        }
+        private const int rowCount = 3, colCount = 3;
+        private XOElement[,] rawValues = new XOElement[rowCount, colCount];
+        private XOElement lastTurn = XOElement.Circle;
+        private XOElement winner = XOElement.None;
+        public event Action<int, int, XOElement> OnTurn;
+        public event Action<XOElement> OnGameOver;
+
+        private XOElement reverseElement(XOElement element)
+        {
+
+            return (XOElement)(-(int)element);
+        }
+
+
+        public bool GameOver
+        {
+            get
+            {
+                    return winner != XOElement.None;
+            }
+        }
+        public XOElement Winner
+        {
+            get
+            {
+                return winner;
+            }
+
+            set
+            {
+                winner = value;
+
+                if(winner !=XOElement.None)
+                {
+                    OnGameOver?.Invoke(winner);
+                }
+            }
+        }
+
+        public bool CanTurn(int row, int col)
+        {
+       
+            return rawValues[row, col] == XOElement.None && !GameOver;
+        }
+
+        public bool TryTurn(int row, int col)
+        {
+            if (!CanTurn(row, col))
+            {
+                return false;
+            }
+
+            rawValues[row, col] = reverseElement(lastTurn);
+            lastTurn = rawValues[row, col];
+            OnTurn?.Invoke(row, col, lastTurn);
+
+            Winner = checkWinner(); 
+            return true;
+        }
+
+        public char[,] Field
+        {
+            get
+            {
+                char[,] result = new char[rowCount, colCount];
+                for (int i = 0; i < rowCount; i++)
+                {
+                    for (int j = 0; j < colCount; j++)
+                    {
+                        if (rawValues[i, j] == XOElement.Cross)
+                            result[i, j] = 'X';
+                        else if (rawValues[i, j] == XOElement.Circle)
+                            result[i, j] = 'O';
+                        else
+                            result[i, j] = '-';
+                    }
+                }
+                return result;
+            }
+        }
+        private XOElement checkRows ()
+        {
+            for(int i=0; i<rowCount; i++)
+            {
+                int sum = 0;
+                for(int j=0; j<colCount; j++)
+                {
+                    sum += (int) rawValues[i, j];
+                }
+                if(sum == 3)
+                {
+                    return XOElement.Cross;
+                }
+                if(sum == -3)
+                {
+                    return XOElement.Circle;
+                }
+            }
+            return XOElement.None;
+        }
+        private XOElement checkWinner ()
+        {
+            var winner = checkRows();
+            if(winner != XOElement.None)
+            {
+                return winner;
+            }
+            winner = checkCols();
+            if(winner != XOElement.None)
+            {
+                return winner;
+            }
+
+            return XOElement.None;
+        }
+        public XOElement checkCols ()
+        {
+            for (int i = 0; i < colCount; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < rowCount; j++)
+                {
+                    sum += (int) rawValues[j, i];
+                }
+                if (sum == 3)
+                {
+                    return XOElement.Cross;
+                }
+                if (sum == -3)
+                {
+                    return XOElement.Circle;
+                }
+            }
+            return XOElement.None;
+        }
+
+    }
+}
+
---
+++ shumatbaev_XOGame/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static shumatbaev_XOGame.Class1;
+
+namespace shumatbaev_XOGame
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow :Window
+    {
+
+        private readonly double rectWidth;
+        private readonly double rectHeight;
+        private readonly Class1 XoGame = new Class1();
+
+        
+        public MainWindow ()
+        {
+            InitializeComponent();
+            XoGame.OnTurn += (row, col, elem) =>
+            {
+                char symbol = elem == XOElement.Cross ? 'X' : 'O';
+                printSymbol(row, col, symbol);
+            };
+            rectWidth = gameField.Width / 3;
+            rectHeight = gameField.Height / 3;
+
+            double y = 0;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                double x = 0;
+
+                for (int j = 1; j <= 3; j++)
+                {
+                    var rect = new Rectangle();
+                    rect.Stroke = Brushes.Black;
+                    rect.StrokeThickness = 1;
+                    rect.Height = rectHeight;
+                    rect.Width = rectWidth;
+
+                    Canvas.SetLeft(rect, x);
+                    Canvas.SetTop(rect, y);
+
+                    gameField.Children.Add(rect);
+
+                    x += rectWidth;
+                }
+                y += rectHeight;
+            }
+        }
+        private (int, int) calcCell (double x, double y)
+        {
+            int row = (int)(y / rectHeight);
+            int col = (int)(x / rectWidth);
+
+            return (row, col);
+        }
+
+        private void printSymbol(int row, int col, char symbol)
+        {
+            (double cornerX, double cornerY) = (rectHeight * col, rectWidth * row);
+
+            Label text = new Label();
+            text.Content = symbol;
+            text.FontSize = rectWidth / 1.5;
+
+            Canvas.SetLeft(text, cornerX + rectWidth / 4);
+            Canvas.SetTop(text, cornerY);
+            gameField.Children.Add(text);
+
+           
+        }
+
+        private void makeTurn(object sender, MouseButtonEventArgs e)
+        {
+            (int row, int col) = calcCell(e.GetPosition(gameField).X, e.GetPosition(gameField).Y);
+            XoGame.TryTurn(row, col);
+        }
+
+    }
+}
